3f7cb5e60e061029813e634ce1299e8bc630fb56
Ghend22 <jguir.22@gmail.com>
2022-04-25 15:25:21 -0500

    Ahora si los tabs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,6 +22,18 @@ public class HomeController : Controller
     {
         return View();
     }
+     public IActionResult Catalogo()
+    {
+        return View();
+    }
+     public IActionResult Contacto()
+    {
+        return View();
+    }
+     public IActionResult Nosotros()
+    {
+        return View();
+    }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
