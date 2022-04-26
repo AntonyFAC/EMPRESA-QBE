8e76ae45acd9627c79b93caeade4bf73d1867df3
missael-xm <xm.missael@gmail.com>
2022-04-26 13:11:14 -0500

    addProducto

--- Models/Producto.cs
+++ Models/Producto.cs
@@ -2,13 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 
 namespace EMPRESA_QBE.Models
 {
+    [Table("t_producto")]
     public class Producto
     {
-        [Table("t_producto")]
-   
+        
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         [Column("id")]
         public int Id { get; set; }
