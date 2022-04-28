eac5abe894a28ae4637ccfda60a4d35b9ee10493
missael-xm <xm.missael@gmail.com>
2022-04-28 13:03:52 -0500

    AddVistas

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -35,6 +35,11 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult Administrador()
+    {
+        return View();
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
